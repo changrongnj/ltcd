d44461e0fb99a15a48c115260d38717d237648e3
Rong <changrongnj@gmail.com>
2019-12-30 19:34:57 -0800

    Update and rename Q1_TwoSum.py to Q001_Q167_TwoSum.py

--- Q001_Q167_TwoSum.py
+++ Q001_Q167_TwoSum.py
@@ -27,3 +27,27 @@ class Solution(object):
                 return(numsDict[num2], index)  #indexs for complement and current
             else:
                 numsDict[num] = index  #if not, stored the visited num
+                
+ 
+'''
+Q167
+167. Two Sum II - Input array is sorted
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+'''
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        remainsDct = {}
+        for index, number in enumerate(numbers, start = 1):
+            if target - number in remainsDct:
+                return remainsDct[target - number], index
+            else:
+                remainsDct[number] = index 
