ba282f296ba24d165366183dc419efff8033e208
Rong <changrongnj@gmail.com>
2020-02-16 20:12:50 -0800

    Update Q0169_MajorityEle.py

--- Q0169_MajorityEle.py
+++ Q0169_MajorityEle.py
@@ -10,6 +10,22 @@ Input: [2,2,1,1,1,2,2]
 Output: 2
 '''
 
+#M1 Moore Algorithm
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        major = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            if count == 0:
+                major = nums[i]
+                count = 1
+            elif nums[i] == major:
+                count += 1
+            else:
+                count -= 1
+        return major
+
+#M2
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         if len(nums) == 0:
@@ -21,7 +37,8 @@ class Solution:
             for i in range(len(nums)):
                 if nums[i] in occurence:
                     occurence[nums[i]] += 1
+                    if occurence[nums[i]] > math.floor(len(nums) / 2):
+                        return nums[i]
                 else:
                     occurence[nums[i]] = 1
-                if occurence[nums[i]] > math.floor(len(nums) / 2):
-                    return nums[i]
+
