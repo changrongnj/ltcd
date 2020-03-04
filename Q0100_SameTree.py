98bc152b45b3fd7a76280c1f2c3769d4c3d1a68d
Rong <changrongnj@gmail.com>
2020-03-03 22:13:27 -0800

    Update Q0100_SameTree.py

--- Q0100_SameTree.py
+++ Q0100_SameTree.py
@@ -52,4 +52,13 @@ class Solution:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
         return p is q
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        while p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         
